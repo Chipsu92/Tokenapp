b67c5b50cf83d777f58509b22684b76377f1be95
Chipsu92 <Wodaryjebanie111>
2018-02-09 00:54:24 +0100

    Last for 06/02

--- src/GUI/Controller.java
+++ src/GUI/Controller.java
@@ -1,4 +1,39 @@
 package GUI;
+
+import javafx.application.Platform;
+import javafx.scene.control.Button;
+
+/*
+fx:id = "anchorPane" - AnchorPane
+fx:id = "headerLabel" - TokenAPP
+fx:id = "startButton" -start button
+fx:id = "statisticButton" - statistic button
+fx:id = "exitButton" - exit button
+fx:id = "creditsLabel" - credits label
+fx:id = "textArea" - text box
+fx:id = "eventArea" -event box
+fx:id = "signinButton" - sign in button
+fx:id = "signinLabel" - sign in label
+fx:id = "textLabel" - textbox label
+fx:id = "terminalLabel" - terminalbox label
+*/
+
+
 //Controls BasicApplication
 public class Controller {
+
+    public Button startButton;
+    public Button  statisticButton;
+    public Button exitButton;
+    public Button signinButton;
+
+   //Handle button click
+    public void handleButtonClick() {
+
+        startButton.setOnAction(e -> System.out.println("Magic"));
+        statisticButton.setOnAction(e -> System.out.println("statistics"));
+        signinButton.setOnAction(e -> System.out.println("sign in"));
+        exitButton.setOnAction(e -> Platform.exit());
+
+    }
 }
--- src/GUI/mainGui.java
+++ src/GUI/mainGui.java
@@ -8,28 +8,12 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-/*
-fx:id " "
-fx:id = "anchorPane" - AnchorPane
-fx:id = "headerLabel" - TokenAPP
-fx:id = "startButton" -start button
-fx:id = "statisticButton" - statistic button
-fx:id = "exitButton" - exit button
-fx:id = "creditsLabel" - credits label
-fx:id = "textArea" - text box
-fx:id = "eventArea" -event box
-fx:id = "signinButton" - sign in button
-fx:id = "signinLabel" - sign in label
-fx:id = "textLabel" - textbox label
-fx:id = "terminalLabel" - terminalbox label
-
-*/
 
 
 
 public class mainGui extends Application {
 
-    Stage window;
+
 
     public static void main(String[] args) {
 
